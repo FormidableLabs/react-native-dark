54a7556107fe17bffb24ed54baf3faf1dd240890
Eli Zibin <elizibin@gmail.com>
2024-04-11 14:38:37 -0700

    Update shim for RN 0.73+
    RN improved the typing of StyleSheet in 0.73 (https://github.com/facebook/react-native/commit/b0a9053e67e60611e398fd70cae2b9887eb42f39) by removing the possibility of typos/invalid keys. It looks like dark never really accounted for that, so using dark on 0.73 causes TS squigglies due to the fact that `dark` is not a valid propery key for any styles! Somewhere along the way too, it did widen some types from being 'flex-start' to 'string' which also caused errors.
    
    This simplifies (and hopefully doesn't break?) the shim, by mostly just lifting the new TS definition inside RN, and applying the `AddDark` type.
    
    I tested this on `vnxl` after upgrading the example app to 0.73 and by patching packages.
    
    Update shim.d.ts

--- shim.d.ts
+++ shim.d.ts
@@ -4,26 +4,20 @@ module "react-native" {
   export * from "react-native";
 
   export namespace StyleSheet {
-    type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
-
     type AddDark<T> = T & { $dark?: T };
-
-    type DarkStyles<T> = {
+    type NamedStylesWithDark<T> = {
       [P in keyof T]:
         | AddDark<ViewStyle>
         | AddDark<TextStyle>
         | AddDark<ImageStyle>;
     };
+    
 
     /**
      * Create a stylesheet with dark-mode support via $dark property on style objects.
      */
-    export function create<T extends DarkStyles<T> | DarkStyles<any>>(
-      styles: T | DarkStyles<T>,
-    ): {
-      [Key in keyof T]: T[Key] extends infer Base & { $dark: infer Dark }
-        ? Expand<Omit<Base, keyof Base & "$dark"> & Partial<Dark>>
-        : T[Key];
-    };
+    export function create<T extends NamedStylesWithDark<T> | NamedStylesWithDark<any>>(
+      styles: T & NamedStylesWithDark<any>,
+    ): T;
   }
 }
